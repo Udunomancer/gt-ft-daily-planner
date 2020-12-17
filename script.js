cf2fbc3be6eff7fdceda6eb8fdf8c2aeb38d9f3f
Udunomancer <kyle717@gmail.com>
2020-12-16 19:53:07 -0500

    Added time rows to block builder

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
 // ===DOM VARIABLES===
 var containEl = $(".container");
-containEl.text("Works");
 
 // ===JS VARIABLES===
 var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 // ===FUNCTION DEFINITIONS===
-// function init() {
-//     var 
+function init() {
+    for (var i = 0; i < hours.length; i++) {
+        var rowEl = $("div");
+        rowEl.attr("class", "row time-block");
+        containEl.append(rowEl);
 
-//     for (var i = 0; i < hours.length; i++) {
-
-//     }
-// }
-// // ===FUNCTION CALLS===
-// init();
+        
+    }
+}
+// ===FUNCTION CALLS===
+init();
 
 // ===EVENT LISTENERS===
 
