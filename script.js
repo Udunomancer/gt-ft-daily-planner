d9e67c3cede145c96e9835dca213392997e41591
Udunomancer <kyle717@gmail.com>
2020-12-16 19:50:02 -0500

    Linked JS file to index.html

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+// ===DOM VARIABLES===
+var containEl = $(".container");
+containEl.text("Works");
+
+// ===JS VARIABLES===
+var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+// ===FUNCTION DEFINITIONS===
+// function init() {
+//     var 
+
+//     for (var i = 0; i < hours.length; i++) {
+
+//     }
+// }
+// // ===FUNCTION CALLS===
+// init();
+
+// ===EVENT LISTENERS===
 
 });
