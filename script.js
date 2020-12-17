b3e24a4109e1fada2ab49010fc5a7753adb06db0
Udunomancer <kyle717@gmail.com>
2020-12-16 20:00:26 -0500

    Added content to rows

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 // ===FUNCTION DEFINITIONS===
 function init() {
     for (var i = 0; i < hours.length; i++) {
-        var rowEl = $("div");
+        var rowEl = $("<div>");
         rowEl.attr("class", "row time-block");
         containEl.append(rowEl);
 
-        
+        var timeEl = $("<div>");
+        timeEl.attr("class", "col-1 hour");
+        rowEl.append(timeEl);
+
+        var scheduleEl = $("<textarea>");
+        scheduleEl.attr("class", "col-10 description");
+        rowEl.append(scheduleEl);
+
+        var buttonEl = $("<button>");
+        buttonEl.attr("class", 'col-1 saveBtn');
+        rowEl.append(buttonEl);
     }
 }
 // ===FUNCTION CALLS===
